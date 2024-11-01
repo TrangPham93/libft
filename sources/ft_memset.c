5a90fc54aec52b8672e0cf424731f00f1897536c
Trang Pham <trpham@c1r4p3.hive.fi>
2024-11-01 19:19:28 +0200

    recommit memset

--- sources/ft_memset.c
+++ sources/ft_memset.c
@@ -6,24 +6,31 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 17:41:10 by trpham            #+#    #+#             */
-/*   Updated: 2024/11/01 18:28:18 by trpham           ###   ########.fr       */
+/*   Updated: 2024/11/01 19:17:58 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memset(char *s, int c, int n)
-{
-	int	i;
+#include "libft.h"
 
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)s;
 	i = 0;
 	while (i < n)
 	{
-		s[i] = c;
+		ptr[i] = (unsigned char)c;
 		i++;
 	}
-	return (s);
+	return (ptr);
 }
-
 /*
+// size_t is the unsigned integer type of the result of sizeof
+// s is a void*, which doesn't support direct indexing. 
+// You need to cast s to a char* first 
+// because you want to set each byte individually.
 #include <stdio.h>
 #include <string.h>
 
