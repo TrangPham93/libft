b3807aadb21901d9b28750425215f66a5c6f2b87
Trang Pham <trpham@c1r4p5.hive.fi>
2024-11-05 18:38:57 +0200

    ft_tolower.c

--- sources/ft_tolower.c
+++ sources/ft_tolower.c
@@ -1,42 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/05 18:09:44 by trpham            #+#    #+#             */
-/*   Updated: 2024/11/05 18:29:05 by trpham           ###   ########.fr       */
+/*   Created: 2024/11/05 18:23:39 by trpham            #+#    #+#             */
+/*   Updated: 2024/11/05 18:38:12 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_toupper(int c)
+int	ft_tolower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		c -= 32;
+	if (c >= 'A' && c <= 'Z')
+		c += 32;
 	return (c);
 }
-
 /* #include <ctype.h>
 #include <stdio.h>
 
 int	main(void)
 {
-	printf("%c\n", toupper('c'));
-	printf("%c\n",ft_toupper('c'));
+	printf("%c\n", tolower('A'));
+	printf("%c\n",ft_tolower('A'));
 
-	printf("%c\n", toupper('K'));
-	printf("%c\n",ft_toupper('K'));
+	printf("%c\n", tolower('k'));
+	printf("%c\n",ft_tolower('k'));
 
-	printf("%s\n", toupper('\0'));
-	printf("%s\n",ft_toupper('\0'));
+	printf("%d\n", tolower('\0'));
+	printf("%d\n",ft_tolower('\0'));
 	
-	printf("%d\n", toupper(5));
-	printf("%d\n",ft_toupper(5));
+	printf("%d\n", tolower(5));
+	printf("%d\n",ft_tolower(5));
 
-	printf("%c\n", toupper('d'));
-	printf("%c\n",ft_toupper('d'));
+	printf("%c\n", tolower('Z'));
+	printf("%c\n",ft_tolower('Z'));
 
 	return (0);
-
 } */
