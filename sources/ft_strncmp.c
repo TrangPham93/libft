a3a64197d9a04eac21cba778dc28b2a2d2554e7e
Trang Pham <trpham@c1r4p5.hive.fi>
2024-11-05 20:27:36 +0200

    ft_strncmp

---
+++ sources/ft_strncmp.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 19:43:57 by trpham            #+#    #+#             */
+/*   Updated: 2024/11/05 20:26:47 by trpham           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+/* #include <string.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*s1 = "howb\n";
+	char	*s2 = "howare";
+	
+	
+	printf("%d\n", strncmp(s1, s2, 3));
+	printf("%d\n", ft_strncmp(s1, s2, 3));
+	printf("\n");
+	printf("%d\n", strncmp(s1, s2, 5));
+	printf("%d\n", ft_strncmp(s1, s2, 5));
+	printf("\n");
+	printf("%d\n", strncmp(s1, s2, 6));
+	printf("%d\n", ft_strncmp(s1, s2, 6));
+	printf("\n");
+	printf("%d\n", strncmp(s1, s2, 8));
+	printf("%d\n", ft_strncmp(s1, s2, 8));
+	printf("\n");
+	printf("%d\n", strncmp(s1, s2, 0));
+	printf("%d\n", ft_strncmp(s1, s2, 0));
+	printf("\n");
+	char	*s3 = "how ";
+	char	*s4 = "howare";
+	printf("%d\n", strncmp(s3, s4, 4));
+	printf("%d\n", ft_strncmp(s3, s4, 4));
+
+	return (0);
+} */
--- sources/libft.h
+++ sources/libft.h
@@ -6,7 +6,7 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 18:54:33 by trpham            #+#    #+#             */
-/*   Updated: 2024/11/05 18:22:55 by trpham           ###   ########.fr       */
+/*   Updated: 2024/11/05 20:27:20 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,14 @@ int		ft_toupper(int c);
 
 // string functions
 int		ft_strlen(char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void	*s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
