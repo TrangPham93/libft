ae1100512039db57fae0e34a4335ea5b32d5a9b5
Trang Pham <trpham@c1r2p5.hive.fi>
2024-11-13 22:30:22 +0200

    commit all

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -6,7 +6,7 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 13:46:48 by trpham            #+#    #+#             */
-/*   Updated: 2024/11/12 13:55:52 by trpham           ###   ########.fr       */
+/*   Updated: 2024/11/13 21:54:25 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,39 +24,3 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (count);
 }
-
-/* #include "stdio.h"
-
-int	main(void)
-{
-	int a = 5;
-	int b = 7;
-	int c = 3;
-	t_list	*a_list = ft_lstnew(&a);
-	t_list	*b_list = ft_lstnew(&b);
-	t_list	*new = ft_lstnew(&c);
-	t_list	*current;
-
-	if (!a_list || !b_list || !new)
-	{
-		printf("memory allocation failed\n");
-		return (1);
-	}
-	
-	a_list ->next = b_list;
-	current = new;
-	ft_lstadd_front(&a_list, new);
-	while (current)
-	{
-		printf("%d\n", *(int *)current->content);
-		current = current->next;
-	}
-	printf("size of list %d\n", ft_lstsize(new));
-	while (new)
-	{
-		t_list	*temp = new;
-		new = new->next;
-		free(temp);
-	}
-	return (0);
-}  */
