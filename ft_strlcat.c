31b572a80d171478984644092d4c4175e2d6821c
Trang Pham <trpham@c1r4p3.hive.fi>
2024-11-18 15:38:22 +0200

    commit all

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 16:26:44 by trpham            #+#    #+#             */
-/*   Updated: 2024/11/18 13:21:54 by trpham           ###   ########.fr       */
+/*   Updated: 2024/11/18 15:14:56 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,14 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	src_len;
 	size_t	dst_len;
 
-	if (!dst || !src)
+	src_len = 0;
+	dst_len = 0;
+	if ((!dst || !src) && size == 0)
 		return (0);
-	src_len = ft_strlen(src);
+	if (src)
+		src_len = ft_strlen(src);
 	dst_len = ft_strlen(dst);
-	if (size == 0)
-		return (src_len);
-	if (dst_len >= size)
+	if (size <= dst_len)
 		return (size + src_len);
 	i = dst_len;
 	j = 0;
